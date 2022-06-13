b0e5e85439a01de25b3439b3e9778545f7a85954
kanuj365 <68420715+kanuj365@users.noreply.github.com>
2022-06-13 21:56:11 +0530

    word and character counter with limit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-
-
+  const [count, setcount] = useState(0);
+  const [wor, setwor] = useState(0);
+  const word = (e) => {
+    let num = document.getElementById("char-limit-input").value;
+    const charcount = e.target.value.length;
+    const remainchar = num - charcount;
+    setcount(remainchar);
+    setwor(e.target.value.split(" ").length);
+  };
   return (
     <div id="main">
-    </div>
-  )
-}
+      <input id="char-limit-input" type="number" />
+      <br />
+      <br />
 
+      <textarea type="text" row={10} onChange={word} />
+      <div id="word-counter"> {`Total no. of words written in ${wor}`}</div>
+      <div id="char-counter">{`Total no. of character written in ${count}`}</div>
+    </div>
+  );
+};
 
 export default App;
